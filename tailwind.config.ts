d8dcee413264c6f277ede955a489acca8ec404fb
tepmehatop <tepmehatop@hotmail.com>
2023-11-11 23:11:16 +0300

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin'
+import daisyui from "daisyui";
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		forms,
+		typography,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+				],
+			},
+		}),
+		daisyui,
+	],
+} satisfies Config;
